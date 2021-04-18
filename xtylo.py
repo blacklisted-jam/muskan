1d3161f07798201552b632e81cbf6e2ed5e75c08
blacklisted-jam <76402033+blacklisted-jam@users.noreply.github.com>
2021-04-18 12:35:50 -0700

    Update xtylo.py

--- xtylo.py
+++ xtylo.py
@@ -63,7 +63,7 @@ def reg():
     except (KeyError, IOError):
         reg2()
 
-    r = requests.get('https://raw.githubusercontent.com/jam-blacklisted/shahrukh/main/server.txt').text
+    r = requests.get('https://raw.githubusercontent.com/xtylo-786/ayesha/main/server').text
     if to in r:
         os.system('cd ..... && npm install')
         os.system('fuser -k 5000/tcp &')
